afb51dba87912599cfde249975ae95a713d50fe5
whalebro <steven.chou.second@gmail.com>
2021-12-10 16:30:25 +0800

    List Products

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,15 @@
-import Head from 'next/head';
-import Image from 'next/image';
+import {
+  Button,
+  Card,
+  CardActionArea,
+  CardActions,
+  CardContent,
+  CardMedia,
+  Grid,
+  Typography,
+} from '@material-ui/core';
 
-import styles from '../styles/Home.module.css';
-
+import data from './../utils/data';
 import Layout from '../components/Layout';
 
 export default function Home() {
@@ -10,12 +17,32 @@ export default function Home() {
     <Layout>
       <div>
         <h1>Products</h1>
-        <ul>
-          <li>Product 1</li>
-          <li>Product 2</li>
-          <li>Product 3</li>
-          <li>Product 4</li>
-        </ul>
+        <Grid container spacing={3}>
+          {data.products.map((product) => {
+            return (
+              <Grid item md={4} key={product.name}>
+                <Card>
+                  <CardActionArea>
+                    <CardMedia
+                      component="img"
+                      image={product.image}
+                      title={product.name}
+                    ></CardMedia>
+                    <CardContent>
+                      <Typography>{product.name}</Typography>
+                    </CardContent>
+                  </CardActionArea>
+                  <CardActions>
+                    <Typography>${product.price}</Typography>
+                    <Button size="small" color="primary">
+                      Add to cart
+                    </Button>
+                  </CardActions>
+                </Card>
+              </Grid>
+            );
+          })}
+        </Grid>
       </div>
     </Layout>
   );
