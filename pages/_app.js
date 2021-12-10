83218244895e339fa5b03b6c4bea0ab5544998f9
whalebro <steven.chou.second@gmail.com>
2021-12-10 16:01:14 +0800

    Fix Material-UI SSR Issue

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,18 @@
-import '../styles/globals.css'
+import { useEffect } from 'react';
+
+import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  useEffect(() => {
+    // refresh 後，
+    const jssStyles = document.querySelector('#jss-server-side');
+    // if (jssStyles) {
+    //   console.log('hello');
+    //   jssStyles.parentElement.removeChild(jssStyles);
+    // }
+  }, []);
+
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
---
+++ pages/_document.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { ServerStyleSheets } from '@material-ui/core/styles';
+import Document, { Head, Html, Main, NextScript } from 'next/document';
+
+export default class MyDocument extends Document {
+  render() {
+    console.log('===> MyDocument');
+    return (
+      <Html lang="en">
+        <Head></Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+MyDocument.getInitialProps = async (ctx) => {
+  console.log('===> server side stylesheets');
+  const sheets = new ServerStyleSheets();
+  const originalRenderPage = ctx.renderPage;
+
+  ctx.renderPage = () => {
+    return originalRenderPage({
+      enhanceApp: (App) => (props) => sheets.collect(<App {...props} />),
+    });
+  };
+  const initialProps = await Document.getInitialProps(ctx);
+
+  return {
+    ...initialProps,
+    styles: [
+      ...React.Children.toArray(initialProps.styles),
+      sheets.getStyleElement(),
+    ],
+  };
+};
