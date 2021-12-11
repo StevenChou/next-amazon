9b3edc65b588c02f122853fa5f4b146ec1e548e5
whalebro <steven.chou.second@gmail.com>
2021-12-11 15:15:11 +0800

    Create React Context

--- components/Layout.js
+++ components/Layout.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import Head from 'next/head';
 // 與 MUI link 做區別
 import NextLink from 'next/link';
@@ -9,14 +9,19 @@ import {
   createMuiTheme,
   CssBaseline,
   Link,
+  Switch,
   ThemeProvider,
   Toolbar,
   Typography,
 } from '@material-ui/core';
+import Cookies from 'js-cookie';
 
 import useStyles from '../utils/styles';
+import { Store } from '../utils/Store';
 
 export default function Layout({ title, description, children }) {
+  const { state, dispatch } = useContext(Store);
+  const { darkMode } = state;
   const theme = createMuiTheme({
     typography: {
       h1: {
@@ -31,7 +36,7 @@ export default function Layout({ title, description, children }) {
       },
     },
     palette: {
-      type: 'light',
+      type: darkMode ? 'dark' : 'light',
       primary: {
         main: '#f0c000',
       },
@@ -41,6 +46,12 @@ export default function Layout({ title, description, children }) {
     },
   });
   const classes = useStyles();
+
+  const darkModeChangeHandler = () => {
+    dispatch({ type: darkMode ? 'DARK_MODE_OFF' : 'DARK_MODE_ON' });
+    const newDarkMode = !darkMode;
+    Cookies.set('darkMode', newDarkMode ? 'ON' : 'OFF');
+  };
 
   return (
     <div>
@@ -59,6 +70,10 @@ export default function Layout({ title, description, children }) {
             </NextLink>
             <div className={classes.grow}></div>
             <div>
+              <Switch
+                checked={darkMode}
+                onChange={darkModeChangeHandler}
+              ></Switch>
               <NextLink href="/cart" passHref>
                 <Link>Cart</Link>
               </NextLink>
