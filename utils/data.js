d61773e19c2e020f13e99cbe30ddcb00cdf557e0
whalebro <steven.chou.second@gmail.com>
2021-12-11 21:17:37 +0800

    Create Sample Users In MongoDB

--- utils/data.js
+++ utils/data.js
@@ -1,4 +1,20 @@
+import bcrypt from 'bcryptjs';
+
 const data = {
+  users: [
+    {
+      name: 'Whalebro',
+      email: 'steven.chou@gmail.com',
+      password: bcrypt.hashSync('123456'),
+      isAdmin: true,
+    },
+    {
+      name: 'Jane',
+      email: 'user@example.com',
+      password: bcrypt.hashSync('123456'),
+      isAdmin: false,
+    },
+  ],
   products: [
     {
       name: 'Free Shirt',
