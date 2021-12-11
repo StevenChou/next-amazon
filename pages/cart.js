2f52f6547837f96b6f86e4ab4b2a2fe02381dad5
whalebro <steven.chou.second@gmail.com>
2021-12-11 19:50:06 +0800

    Update Remove Cart Items

--- pages/cart.js
+++ pages/cart.js
@@ -20,15 +20,29 @@ import {
   List,
   ListItem,
 } from '@material-ui/core';
+import axios from 'axios';
 
 import { Store } from '../utils/Store';
 import Layout from '../components/Layout';
 
 function CartScreen() {
-  const { state } = useContext(Store);
+  const { state, dispatch } = useContext(Store);
   const {
     cart: { cartItems },
   } = state;
+
+  const updateCartHandler = async (item, quantity) => {
+    const { data } = await axios.get(`/api/products/${item._id}`);
+    if (data.countInStock < quantity) {
+      window.alert('Sorry. Product is out of stock');
+      return;
+    }
+
+    dispatch({ type: 'CART_ADD_ITEM', payload: { ...item, quantity } });
+  };
+  const removeItemHandler = (item) => {
+    dispatch({ type: 'CART_REMOVE_ITEM', payload: item });
+  };
 
   return (
     <Layout title="Shopping Cart">
@@ -37,7 +51,10 @@ function CartScreen() {
       </Typography>
       {cartItems.length === 0 ? (
         <div>
-          Cart is empty. <NextLink href="/">Go shopping</NextLink>
+          Cart is empty.{' '}
+          <NextLink href="/" passHref>
+            <Link>Go shopping</Link>
+          </NextLink>
         </div>
       ) : (
         <Grid container spacing={1}>
@@ -77,7 +94,12 @@ function CartScreen() {
                         </NextLink>
                       </TableCell>
                       <TableCell align="right">
-                        <Select value={item.quantity}>
+                        <Select
+                          value={item.quantity}
+                          onChange={(e) =>
+                            updateCartHandler(item, e.target.value)
+                          }
+                        >
                           {[...Array(item.countInStock).keys()].map((x) => (
                             <MenuItem key={x + 1} value={x + 1}>
                               {x + 1}
@@ -87,7 +109,11 @@ function CartScreen() {
                       </TableCell>
                       <TableCell align="right">${item.price}</TableCell>
                       <TableCell align="right">
-                        <Button variant="contained" color="secondary">
+                        <Button
+                          variant="contained"
+                          color="secondary"
+                          onClick={() => removeItemHandler(item)}
+                        >
                           x
                         </Button>
                       </TableCell>
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,7 @@
+import { useContext } from 'react';
+import { useRouter } from 'next/router';
 import NextLink from 'next/link';
+
 import {
   Button,
   Card,
@@ -9,14 +12,34 @@ import {
   Grid,
   Typography,
 } from '@material-ui/core';
+import axios from 'axios';
 
 // import data from './../utils/data';
-import Layout from '../components/Layout';
 import db from '../utils/db';
+import Layout from '../components/Layout';
 import Product from '../models/Product';
+import { Store } from '../utils/Store';
 
 export default function Home(props) {
+  const router = useRouter();
+  const { state, dispatch } = useContext(Store);
+
   const { products } = props;
+
+  const addToCartHandler = async (product) => {
+    const existItem = state.cart.cartItems.find((x) => x._id === product._id);
+    const quantity = existItem ? existItem.quantity + 1 : 1;
+    const { data } = await axios.get(`/api/products/${product._id}`);
+
+    if (data.countInStock < quantity) {
+      window.alert('Sorry. Product is out of stock');
+      return;
+    }
+
+    dispatch({ type: 'CART_ADD_ITEM', payload: { ...product, quantity } });
+
+    router.push('/cart');
+  };
 
   return (
     <Layout>
@@ -41,7 +64,11 @@ export default function Home(props) {
                   </NextLink>
                   <CardActions>
                     <Typography>${product.price}</Typography>
-                    <Button size="small" color="primary">
+                    <Button
+                      size="small"
+                      color="primary"
+                      onClick={() => addToCartHandler(product)}
+                    >
                       Add to cart
                     </Button>
                   </CardActions>
