dc3d82bd97597a528c66ac61f1b5861694b9eb14
whalebro <steven.chou.second@gmail.com>
2021-12-12 12:36:40 +0800

    Create Register Page

--- pages/register.js
+++ pages/register.js
@@ -1,7 +1,9 @@
 import React, { useContext, useEffect, useState } from 'react';
-import NextLink from 'next/link';
 import { useRouter } from 'next/router';
+import NextLink from 'next/link';
 
+import axios from 'axios';
+import Cookies from 'js-cookie';
 import {
   List,
   ListItem,
@@ -10,35 +12,39 @@ import {
   Button,
   Link,
 } from '@material-ui/core';
-import axios from 'axios';
-import Cookies from 'js-cookie';
 
 import { Store } from '../utils/Store';
 import Layout from '../components/Layout';
 import useStyles from '../utils/styles';
 
-export default function Login() {
+export default function Register() {
   const router = useRouter();
+  const { redirect } = router.query;
   const { state, dispatch } = useContext(Store);
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const { userInfo } = state;
   useEffect(() => {
     if (userInfo) {
       router.push('/');
     }
   }, []);
 
-  const { redirect } = router.query; // login?redirect=/shipping
-  const { userInfo } = state;
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
   const classes = useStyles();
-
-  console.log('****===> login');
 
   const submitHandler = async (e) => {
     e.preventDefault();
 
+    if (password !== confirmPassword) {
+      alert("passwords don't match");
+      return;
+    }
+
     try {
-      const { data } = await axios.post('/api/users/login', {
+      const { data } = await axios.post('/api/users/register', {
+        name,
         email,
         password,
       });
@@ -54,12 +60,22 @@ export default function Login() {
   };
 
   return (
-    <Layout title="Login">
-      <form className={classes.form} onSubmit={submitHandler}>
+    <Layout title="Register">
+      <form onSubmit={submitHandler} className={classes.form}>
         <Typography component="h1" variant="h1">
-          Login
+          Register
         </Typography>
         <List>
+          <ListItem>
+            <TextField
+              variant="outlined"
+              fullWidth
+              id="name"
+              label="Name"
+              inputProps={{ type: 'text' }}
+              onChange={(e) => setName(e.target.value)}
+            ></TextField>
+          </ListItem>
           <ListItem>
             <TextField
               variant="outlined"
@@ -81,14 +97,24 @@ export default function Login() {
             ></TextField>
           </ListItem>
           <ListItem>
+            <TextField
+              variant="outlined"
+              fullWidth
+              id="confirmPassword"
+              label="Confirm Password"
+              inputProps={{ type: 'password' }}
+              onChange={(e) => setConfirmPassword(e.target.value)}
+            ></TextField>
+          </ListItem>
+          <ListItem>
             <Button variant="contained" type="submit" fullWidth color="primary">
-              Login
+              Register
             </Button>
           </ListItem>
           <ListItem>
-            Don't have an account? &nbsp;
-            <NextLink href={`/register?redirect=${redirect || '/'}`} passHref>
-              <Link>Register</Link>
+            Already have an account? &nbsp;
+            <NextLink href={`/login?redirect=${redirect || '/'}`} passHref>
+              <Link>Login</Link>
             </NextLink>
           </ListItem>
         </List>
