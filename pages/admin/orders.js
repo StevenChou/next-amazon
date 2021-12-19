df6d2f7e74ef0033e51ec5a6797c8bac06dc7aa2
whalebro <steven.chou.second@gmail.com>
2021-12-19 17:38:40 +0800

    List Users

--- pages/admin/orders.js
+++ pages/admin/orders.js
@@ -40,7 +40,7 @@ function reducer(state, action) {
   }
 }
 
-function AdminDashboard() {
+function AdminOrders() {
   const { state } = useContext(Store);
   const router = useRouter();
   const classes = useStyles();
@@ -91,6 +91,11 @@ function AdminDashboard() {
               <NextLink href="/admin/products" passHref>
                 <ListItem button component="a">
                   <ListItemText primary="Products"></ListItemText>
+                </ListItem>
+              </NextLink>
+              <NextLink href="/admin/users" passHref>
+                <ListItem button component="a">
+                  <ListItemText primary="Users"></ListItemText>
                 </ListItem>
               </NextLink>
             </List>
@@ -163,4 +168,4 @@ function AdminDashboard() {
   );
 }
 
-export default dynamic(() => Promise.resolve(AdminDashboard), { ssr: false });
+export default dynamic(() => Promise.resolve(AdminOrders), { ssr: false });
