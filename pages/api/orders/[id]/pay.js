194336e94dec2f367cdeea06e5ef41151dec7719
whalebro <steven.chou.second@gmail.com>
2021-12-26 17:38:20 +0800

    fixed default export

--- pages/api/orders/[id]/pay.js
+++ pages/api/orders/[id]/pay.js
@@ -1,7 +1,7 @@
 import nc from 'next-connect';
 
 import db from '../../../../utils/db';
-import onError from '../../../../utils/error';
+import { onError } from '../../../../utils/error';
 import { isAuth } from '../../../../utils/auth';
 import Order from '../../../../models/Order';
 
